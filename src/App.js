a8442f9e289bac105098c13aea36a37381420611
yasmins021 <109712172+yasmins021@users.noreply.github.com>
2022-11-06 14:21:43 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -39,7 +39,8 @@ return (
 {/* 3. List of items (unordered list with list items)*/}
  <ul>
  {items.map(item => {
-return ( <li key={item.id}>{item.value} <button onClick={()=> deleteItem(item.id)}>✗</button></li>)
+return ( 
+  <li key={item.id}>{item.value} <button onClick={()=> deleteItem(item.id)}>✗</button></li>)
  })}
  </ul>
    </div>
