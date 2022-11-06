8cd1fd1a3a8eb98ef4a6dda59d648e39d8a51696
yasmins021 <109712172+yasmins021@users.noreply.github.com>
2022-11-06 14:24:40 -0800

    fixed some bugs

--- src/App.js
+++ src/App.js
@@ -1,50 +1,111 @@
-import React,{ useState } from 'react';
-import './App.css';
- 
+import React, { useState } from "react";
+import "./App.css";
+
 function App() {
- //State hook - `useState`
- const [newItem, setNewItem] = useState("");
-const [items,setItems] = useState ([]);
- 
- //helper functions
- function addItem(){
-if (!newItem){
-  alert("Enter an item...")
-  return;
-}
-const item={
-  id: Math.floor (Math.random() * 1000),
-  value:newItem
-};
-setItems(oldList=>[...oldList,item]);
-setNewItem("");
-}
-function deleteItem(id) {
-const newArray = items.filter(item => item.id !== id); 
-setItems(newArray);
+  // State Hook - `useState`
+  const [newItem, setNewItem] = useState("");
+  const [items, setItems] = useState([]);
+
+  const [showEdit, setShowEdit] = useState(-1);
+  const [updatedText, setUpdatedText] = useState("");
+
+  // Helper Functions
+
+  /* Adds a new item to the list array*/
+  function addItem() {
+    // ! Check for empty item
+    if (!newItem) {
+      alert("Press enter an item.");
+      return;
+    }
+
+    const item = {
+      id: Math.floor(Math.random() * 1000),
+      value: newItem,
+    };
+
+    // Add new item to items array
+    setItems((oldList) => [...oldList, item]);
+
+    // Reset newItem back to original state
+    setNewItem("");
+  }
+
+  /* Deletes an item based on the `item.id` key */
+  function deleteItem(id) {
+    const newArray = items.filter((item) => item.id !== id);
+    setItems(newArray);
+  }
+
+  /* Edit an item text after creating it. */
+  function editItem(id, newText) {
+    // Get the current item
+    const currentItem = items.filter((item) => item.id === id);
+
+    // Create a new item with same id
+    const newItem = {
+      id: currentItem.id,
+      value: newText,
+    };
+
+    deleteItem(id);
+
+    // Replace item in the item list
+    setItems((oldList) => [...oldList, newItem]);
+    setUpdatedText("");
+    setShowEdit(-1);
+  }
+
+  // Main part of app
+  return (
+    <div className="app">
+      {/* 1. Header  */}
+      <h1>My Todo List</h1>
+
+      {/* 2. Add new item (input) */}
+      <input
+        type="text"
+        placeholder="Add an item..."
+        value={newItem}
+        onChange={(e) => setNewItem(e.target.value)}
+      />
+
+      {/* Add (button) */}
+      <button onClick={() => addItem()}>Add</button>
+
+      {/* 3. List of todos (unordered list) */}
+      <ul>
+        {items.map((item) => {
+          return (
+            <div>
+              <li key={item.id} onClick={() => setShowEdit(item.id)}>
+                {item.value}
+                <button
+                  className="delete-button"
+                  onClick={() => deleteItem(item.id)}
+                >
+                  ❌
+                </button>
+              </li>
+
+              {showEdit == item.id ? (
+                <div>
+                  <input
+                    type="text"
+                    value={updatedText}
+                    onChange={(e) => setUpdatedText(e.target.value)}
+                  />
+                  <button onClick={() => editItem(item.id, updatedText)}>
+                    Update
+                  </button>
+                </div>
+              ) : null}
+            </div>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
 
-return (
-<div className="App">
-  {/* 1. Header */}
-  <h1>Todo List App</h1>
-  {/* 2. Input (input and button*/}
-  <input
-    type="text"
-    placeholder='Add an item...'
-    value={newItem}
-    onChange={e => setNewItem (e.target.value)}
-    />
-    <button onClick={()=> addItem}>Add</button>
-{/* 3. List of items (unordered list with list items)*/}
- <ul>
- {items.map(item => {
-return ( 
-  <li key={item.id}>{item.value} <button onClick={()=> deleteItem(item.id)}>✗</button></li>)
- })}
- </ul>
-   </div>
- );
-}
- 
 export default App;
