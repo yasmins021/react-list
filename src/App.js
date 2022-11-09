7bc494185f539415f82f155bf7f29c27acf48a16
yasmins021 <109712172+yasmins021@users.noreply.github.com>
2022-11-08 17:40:36 -0800

    added css

--- src/App.js
+++ src/App.js
@@ -74,7 +74,7 @@ function App() {
       <button onClick={() => addItem()}>Add</button>
 
       {/* 3. List of todos (unordered list) */}
-      <ul>
+      <ul className="pixelbullets">
         {items.map((item) => {
           return (
             <div>
