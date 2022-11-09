24ed642ea0f90ac55f4f7f6406866984722f0fdc
yasmins021 <109712172+yasmins021@users.noreply.github.com>
2022-11-08 18:12:46 -0800

    added cute x

--- src/App.js
+++ src/App.js
@@ -84,7 +84,7 @@ function App() {
                   className="delete-button"
                   onClick={() => deleteItem(item.id)}
                 >
-                 <img src=" https://icons8.com/icon/80339/close" alt="X icon"></img>
+                 <img src="close.png " alt="X icon"></img>
                 </button>
               </li>
 
